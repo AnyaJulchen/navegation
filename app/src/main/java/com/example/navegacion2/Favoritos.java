0d96d6bc8486cd39ccc42522274c7de737480b12
seba <sebastian@jmconstructoraspa.cl>
2025-09-09 19:38:47 -0300

    Nav Primera Parte

---
+++ app/src/main/java/com/example/navegacion2/Favoritos.java
@@ -0,0 +1,50 @@
+package com.example.navegacion2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class Favoritos extends Fragment {
+
+    private EditText text_1, text_2;
+    private Button btn;
+
+
+
+    public Favoritos() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_favoritos, container, false);
+
+        text_1 = view.findViewById(R.id.text1);
+        text_2 = view.findViewById(R.id.text2);
+        btn = view.findViewById(R.id.button);
+
+        btn.setOnClickListener( v ->{
+            int num1 = Integer.parseInt(text_1.getText().toString());
+            int num2 = Integer.parseInt(text_2.getText().toString());
+
+        });
+
+
+
+    }
+}
